5ab286ac12068566e9853c0b7d153a041bca4b2e
ronald890119 <ronald890119@gmail.com>
2026-05-09 02:43:42 +1000

    format: formatted the code

--- src/component/Tabs.tsx
+++ src/component/Tabs.tsx
@@ -1,44 +1,42 @@
-import { useDispatch, useSelector } from "react-redux";
-import Badge from "./Badge";
-import { toggleMygoTabClicked } from "../state/slice";
-import type { RootState, AppDispatch } from "../state/store";
+import { useDispatch, useSelector } from 'react-redux'
+import Badge from './Badge'
+import { toggleMygoTabClicked } from '../state/slice'
+import type { RootState, AppDispatch } from '../state/store'
 
 const Tabs = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const mygoTabClicked = useSelector(
-    (state: RootState) => state.content.mygoTabClicked,
-  );
+  const dispatch = useDispatch<AppDispatch>()
+  const mygoTabClicked = useSelector((state: RootState) => state.content.mygoTabClicked)
 
   const handleClick = () => {
-    dispatch(toggleMygoTabClicked());
-  };
+    dispatch(toggleMygoTabClicked())
+  }
 
   return (
     <>
       <button
         onClick={handleClick}
-        className={`inline-block p-4 border-b-2 rounded-t-lg hover:cursor-pointer transition-colors duration-300 ease-in-out ${
+        className={`inline-block rounded-t-lg border-b-2 p-4 transition-colors duration-300 ease-in-out hover:cursor-pointer ${
           mygoTabClicked
-            ? "border-transparent hover:text-gray-300"
-            : "border-blue-500 text-blue-500"
+            ? 'border-transparent hover:text-gray-300'
+            : 'border-blue-500 text-blue-500'
         }`}
       >
         Ave Mujica
-        <Badge category={"ave_mujica"} />
+        <Badge category={'ave_mujica'} />
       </button>
       <button
         onClick={handleClick}
-        className={`inline-block p-4 border-b-2 rounded-t-lg hover:cursor-pointer transition-colors duration-300 ease-in-out ${
+        className={`inline-block rounded-t-lg border-b-2 p-4 transition-colors duration-300 ease-in-out hover:cursor-pointer ${
           mygoTabClicked
-            ? "border-blue-500 text-blue-500"
-            : "border-transparent hover:text-gray-300"
+            ? 'border-blue-500 text-blue-500'
+            : 'border-transparent hover:text-gray-300'
         }`}
       >
         MyGO!!!!!
-        <Badge category={"mygo"} />
+        <Badge category={'mygo'} />
       </button>
     </>
-  );
-};
+  )
+}
 
-export default Tabs;
+export default Tabs
